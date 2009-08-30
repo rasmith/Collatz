6f8ed01e3ba4d5be3b4b5065f8204e47cafa0f81
Randall Smith <rhennig@gmail.com>
2009-08-29 22:55:10 -0500

    added a Collatz.cpp

---
+++ Collatz.cpp
@@ -0,0 +1,90 @@
+/*
+ * Collatz.cpp
+ *
+ *  Created on: Aug 28, 2009
+ *      Author: rsmith
+ */
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+char buf[8];
+unsigned int idx=0;
+char ch;
+
+unsigned int read_num() {
+	idx=0;
+	while(isdigit((ch=getchar()))) {
+		buf[idx++]=ch;
+	}
+	buf[idx++]='\n';
+	buf[idx]='\0';
+	return atoi(buf);
+}
+
+unsigned int get_cached_len(unsigned int n) {
+	return 0;
+}
+unsigned int get_cycle_len(unsigned int n) {
+	unsigned int l = get_cached_len(n);
+	if(l==0) {
+		l=compute_cycle_len(n);
+	}
+	return l;
+}
+
+unsigned  int compute_cycle_len(unsigned int n) {
+	unsigned int len=1;
+	unsigned int m=n;
+	while(m!=1) {
+		//printf("m=%u, len=%u\n",m,len);
+		if( (m & 0x1)  == 0) {
+			m = m >> 1;
+			len++;
+		} else {
+			m = (m << 1) + m + 1;
+			len++;
+		}
+	}
+	return len;
+}
+
+unsigned long int compute_max_cycle_len(unsigned int min, unsigned int max) {
+	unsigned int maxlen=0;
+	unsigned int curlen=0;
+	for(unsigned int i=min;i<=max;i++) {
+		curlen = get_cycle_len(i);
+		//printf("curlen=%u, maxlen=%u\n",curlen,maxlen);
+		if(curlen>maxlen) {
+			maxlen=curlen;
+		}
+	}
+	return maxlen;
+}
+
+int main (int argc, char ** argv) {
+	bool done = false;
+	unsigned  int min;
+	unsigned  int max;
+	unsigned  int min0;
+	unsigned  int max0;
+	unsigned  int  maxlen;
+	unsigned  int temp;
+	while(!done) {
+		min= (unsigned  int)read_num();
+		max = (unsigned int)read_num();
+		min0=min;
+		max0=max;
+		if(min > max) {
+			temp = min;
+			min = max;
+			max = temp;
+		}
+		maxlen =compute_max_cycle_len(min,max);
+		printf("%u %u %u\n",min0,max0,maxlen);
+		done = (ch == EOF);
+	}
+	return 0;
+}
