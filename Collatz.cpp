2fb4c27afcf0627d6a4eaa2820037623c1ce0716
Randall Smith <rhennig@gmail.com>
2009-08-31 16:24:26 -0500

    implemented simple caching

--- Collatz.cpp
+++ Collatz.cpp
@@ -10,32 +10,77 @@
 #include <stdio.h>
 #include <ctype.h>
 
+typedef unsigned int uint;
+
+struct entry  {
+	uint value;
+	uint cycle_len;
+	uint count;
+};
+
+
+
 char buf[8];
-unsigned int idx=0;
+uint idx=0;
 char ch;
 
-unsigned int compute_cycle_len(unsigned int);
+#define CACHE_MASK 0x1FFFF
+#define CACHE_SIZE ( (CACHE_MASK)+1)
+#define BUCKET_SIZE 4
+#define CACHEINDEX(n) (n & (CACHE_MASK))
 
-unsigned int get_cached_len(unsigned int n) {
-	return 0;
+entry cache[CACHE_SIZE];
+
+uint compute_cycle_len(uint);
+//uint lens[600];
+//uint misses=0;
+//uint hits=0;
+
+inline bool lookup_cached_len(uint n) {
+	return (cache[ CACHEINDEX(n) + 0].value  == n);
 }
 
-unsigned int get_cycle_len(unsigned int n) {
-	unsigned int l = get_cached_len(n);
+inline uint get_cached_len(uint n) {
+	uint result=0;
+	uint idx = CACHEINDEX(n);
+	if(n==cache[idx].value) {
+		//cache[idx].count++;
+		result=cache[idx].cycle_len;
+	}
+	//	hits++;
+	//} else {
+	//	misses++;
+	//}
+	return result;
+}
+
+inline uint put_cached_len(uint n, uint l) {
+	uint idx =  CACHEINDEX(n);
+	if(l > cache[idx].cycle_len) {
+		cache[idx].cycle_len=l;
+		cache[idx].value=n;
+	}
+}
+
+uint get_cycle_len(uint n) {
+	uint l = get_cached_len(n);
 	if(l==0) {
 		l=compute_cycle_len(n);
-	}
+		put_cached_len(n,l);
+	} 
 	return l;
 }
 
-unsigned  int compute_cycle_len(unsigned int n) {
-	unsigned int len=1;
-	unsigned int m=n;
+uint compute_cycle_len(uint n) {
+	uint len=1;
+	uint m=n;
 	while(m!=1) {
-		//printf("m=%u, len=%u\n",m,len);
-		if( (m & 0x1)  == 0) {
-			m = m >> 1;
-			len++;
+		if(lookup_cached_len(m)) {
+			return len+get_cached_len(m)-1;
+		} else
+		if( (m & 0x1)  == 0) {		
+				m = m >> 1;
+				len++;
 		} else {
 			m = (m << 1) + m + 1;
 			len++;
@@ -44,11 +89,12 @@ unsigned  int compute_cycle_len(unsigned int n) {
 	return len;
 }
 
-unsigned long int compute_max_cycle_len(unsigned int min, unsigned int max) {
-	unsigned int maxlen=0;
-	unsigned int curlen=0;
-	for(unsigned int i=min;i<=max;i++) {
+uint compute_max_cycle_len(uint min, uint max) {
+	uint maxlen=0;
+	uint curlen=0;
+	for(uint i=min;i<=max;i++) {
 		curlen = get_cycle_len(i);
+	//	lens[curlen]++;
 		//printf("curlen=%u, maxlen=%u\n",curlen,maxlen);
 		if(curlen>maxlen) {
 			maxlen=curlen;
@@ -58,12 +104,13 @@ unsigned long int compute_max_cycle_len(unsigned int min, unsigned int max) {
 }
 
 int main (int argc, char ** argv) {
-	unsigned  int min;
-	unsigned  int max;
-	unsigned  int min0;
-	unsigned  int max0;
-	unsigned  int  maxlen;
-	unsigned  int temp;
+	uint min;
+	uint max;
+	uint min0;
+	uint max0;
+	uint  maxlen;
+	uint temp;
+	
 	while(scanf("%u %u",&min,&max)==2) {
 		min0=min;
 		max0=max;
@@ -74,7 +121,9 @@ int main (int argc, char ** argv) {
 		}
 		maxlen =compute_max_cycle_len(min,max);
 		printf("%u %u %u\n",min0,max0,maxlen);
-		
+	//	printf("%d %d",misses,hits);
 	}
+	
+	
 	return 0;
 }
